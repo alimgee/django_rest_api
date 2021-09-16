c229d5d4da40364d2cda60af3d05e91dfcddd15d
alimgee <mcgee.al@gmail.com>
2021-09-16 16:34:27 +0000

    Adding post list view api

--- posts/serializers.py
+++ posts/serializers.py
@@ -5,6 +5,21 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 *2:
+            raise serializers.ValidationError(
+                'Image size larger than 2MB!'
+        )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px'
+        )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px'
+        )
+        return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
